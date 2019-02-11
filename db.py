1347123756a5fd282082e39a9151fcea5e827e5a
clogic <choiseungil29@gmail.com>
2019-02-11 10:34:11 +0000

    update

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from config import Config
 
-engine = create_engine(f'postgresql+psycopg2://{Config.DB_USERNAME}:{Config.DB_PASSWORD}@{Config.DB_HOST}:{Config.DB_PORT}/{Config.DB_NAME}')
+engine = create_engine(f'postgresql+psycopg2://{Config.DB_USERNAME}:{Config.DB_PASSWORD}@{Config.DB_HOST}:{Config.DB_PORT}/{Config.DB_NAME}', pool_size=50, max_overflow=0)
 session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base(bind=engine)
